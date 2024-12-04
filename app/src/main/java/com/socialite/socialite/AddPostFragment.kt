00e8be27a21b106550347327230d49fb8581bd7c
LainieRainbolt <124300960+LainieRainbolt@users.noreply.github.com>
2024-12-04 16:51:09 -0600

    ADDED: image picker

--- app/src/main/java/com/socialite/socialite/AddPostFragment.kt
+++ app/src/main/java/com/socialite/socialite/AddPostFragment.kt
@@ -10,6 +10,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.fragment.app.Fragment
@@ -36,20 +38,20 @@ class AddPostFragment : Fragment() {
         val commentDatabase: CommentDatabase = CommentDatabase()
 
 
-//        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-//            // Callback is invoked after the user selects a media item or closes the photo picker.
-//            if (uri != null) {
-//                val testEvent: Event = Event()
-//                val context: Context = this;
-//                val bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(this.contentResolver, uri))
-//                val imageView: ImageView? = eventImage
-//                if (imageView != null) {
-//                    imageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 100, 100, false))
-//                }
-//            } else {
-//                Log.d("PhotoPicker", "No media selected")
-//            }
-//        }
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the photo picker.
+            if (uri != null) {
+                val testEvent: Event = Event()
+                val context: Context = this.requireContext()
+                val bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(this.requireContext().contentResolver, uri))
+                val imageView: ImageView? = eventImage
+                if (imageView != null) {
+                    imageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 100, 100, false))
+                }
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
 
         view?.findViewById<FloatingActionButton>(R.id.postEventButton)?.setOnClickListener {
             val title = eventTitleEditText?.text.toString()
@@ -70,6 +72,16 @@ class AddPostFragment : Fragment() {
             eventImage?.setImageResource(R.drawable.ic_launcher_background)
             eventDateEditText?.text?.clear()
             eventDescriptionEditText?.text?.clear()
+
+            Toast.makeText(requireContext(), "Event created!", Toast.LENGTH_SHORT).show()
+
+            //navigate to home fragment
+            //findNavController().navigate(R.id.action_addPostFragment_to_homeFragment)
+        }
+
+
+        eventImage?.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         return view
